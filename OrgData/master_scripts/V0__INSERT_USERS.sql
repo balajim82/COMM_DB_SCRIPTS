2f0298e1815356b2ac91207e6da133449d3da48c
balajim82 <modadugu.balaji@gmail.com>
2026-04-23 18:35:54 +0530

    updated

--- OrgData/master_scripts/V0__INSERT_USERS.sql
+++ OrgData/master_scripts/V0__INSERT_USERS.sql
@@ -1,3 +1,3 @@
-INSERT INTO "OrgData".users_details VALUES (1,'Balaji')
-INSERT INTO "OrgData".users_details VALUES (2,'Balaji-1')
-INSERT INTO "OrgData".users_details VALUES (2,'Balaji-2')
+INSERT INTO users_details VALUES (1,'Balaji')
+INSERT INTO users_details VALUES (2,'Balaji-1')
+INSERT INTO users_details VALUES (2,'Balaji-2')
