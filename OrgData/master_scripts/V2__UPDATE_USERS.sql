34c89304b28edc9bd895b148495f5d306459319a
balajim82 <modadugu.balaji@gmail.com>
2026-04-24 10:47:34 +0530

    updated

---
+++ OrgData/master_scripts/V2__UPDATE_USERS.sql
@@ -0,0 +1,4 @@
+INSERT INTO users_details (usr_id, usr_name) VALUES (3,'Rithvik');
+INSERT INTO users_details (usr_id, usr_name) VALUES (4,'Raji');
+INSERT INTO users_details (usr_id, usr_name) VALUES (5,'Ravi');
+UPDATE users_details SET usr_name = 'Rushaanth' WHERE usr_id=2;
