baf5a671e27b0135a62849622d55a4f407d7df9d
balajim82 <modadugu.balaji@gmail.com>
2026-04-28 15:16:53 +0530

    Updated

--- OrgData/master_scripts/V3__INSERT_USERS.sql
+++ OrgData/master_scripts/V3__INSERT_USERS.sql
@@ -1,2 +1,2 @@
-INSERT INTO table_name (usr_id, usr_name,USR_DATA) 
-VALUES (5, 'Rajasaab', '{"age": 30, "city": "New York", "active": true}');
+INSERT INTO users_details (usr_id, usr_name,USR_DATA) 
+VALUES (6, 'Rajasaab', '{"age": 30, "city": "New York", "active": true}');
